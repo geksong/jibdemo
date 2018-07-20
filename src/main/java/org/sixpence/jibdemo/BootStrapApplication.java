1c5401815ef705934d3e24379c31ec724ced1856
liubianshi <liubianshi@wacai.com>
2018-07-20 17:03:10 +0800

    init

---
+++ src/main/java/org/sixpence/jibdemo/BootStrapApplication.java
@@ -0,0 +1,10 @@
+package org.sixpence.jibdemo;
+
+/**
+ * Created by bianshi on 2018/7/20.
+ */
+public class BootStrapApplication {
+    public static void main(String[] args) {
+        System.out.println("...");
+    }
+}
